bd639b6b8408fa61760093159ff0bdc82c2ecaf9
Eder Natan <102772770+edernatanzz@users.noreply.github.com>
2024-10-15 17:01:30 +0000

    operadores

---
+++ LPI/lista01.py
@@ -0,0 +1,14 @@
+#$nome não é variavel devido aos caracteres
+# (x) pode ser uma variavel
+# 1colocado é variavel
+def q02 ():
+    notaAluno =(int(input('nota do aluno'))) 
+    nomeAluno = (input('nome do aluno'))
+    matAluno = (input('materia'))
+    sexo = input('sexo')
+    print('o aluno se chama : ', nomeAluno, 'tem nota ', notaAluno, 'na materia ' ,  matAluno, 'do sexo ', sexo )
+
+def q03 ():
+    print(2*4 == 24/3)
+    print(15%4 < 19/6)
+
